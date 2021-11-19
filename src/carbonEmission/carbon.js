dfd24a267134ff331beedee121aedb5975678413
Yoonha Kim <yoon.ha.kim@sap.com>
2021-11-19 12:51:29 -0800

    carbon footprint skeleton

---
+++ src/carbonEmission/carbon.js
@@ -0,0 +1,7 @@
+/* carbon footprint
+*  all measures are grams per kilometer, refer to https://ourworldindata.org/travel-carbon-footprint
+*  drivingPerKm  is Medium car(petrol)
+*  transitPerKm is Bus*/
+
+let drivingPerKm = 192
+let transitPerKim  = 105
