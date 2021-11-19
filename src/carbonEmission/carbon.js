ac7ffc2296bb1ee62b97386515212d9ac6cf6d93
Yoonha Kim <yoon.ha.kim@sap.com>
2021-11-19 13:24:11 -0800

    carbon footprint skeleton

--- src/carbonEmission/carbon.js
+++ src/carbonEmission/carbon.js
@@ -1,7 +1,36 @@
 /* carbon footprint
 *  all measures are grams per kilometer, refer to https://ourworldindata.org/travel-carbon-footprint
 *  drivingPerKm  is Medium car(petrol)
-*  transitPerKm is Bus*/
+*  transitPerKm is Bus
+*  Over short to medium distances, walking or cycling are nearly always the lowest carbon way to travel.
+*  While not in the chart, the carbon footprint of cycling one kilometer is usually
+*  in the range of 16 to 50 grams CO2eq per km depending on how efficiently you cycle and what you eat.
+*  so we set walkPerKm and cyclePerKm as the average of the range*/
 
-let drivingPerKm = 192
-let transitPerKim  = 105
+
+const drivingPerKm = 192;
+const transitPerKm  = 105;
+const walkPerKm = (16 + 50)/2;
+const cyclePerKm = (16 + 50)/2;
+
+
+class Carbon {
+    constructor(){
+        this.carbon = 0;
+    }
+
+    getCarbonFootPrint(drivingType, distance){
+        switch (drivingType) {
+            case driving:
+                return drivingPerKm*distance;
+                break;
+            case transit:
+                return transitPerKm*distance;
+            case walk:
+                return walkPerKm*distance;
+            case cycle:
+                return cyclePerKm*distance;
+        }
+    }
+}
+
