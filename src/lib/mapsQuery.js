f4266b89b93c7ad18d92a6960f158828c183d310
lian-d <lianduan64@gmail.com>
2021-11-19 13:27:33 -0800

    map data template

--- src/lib/mapsQuery.js
+++ src/lib/mapsQuery.js
@@ -3,21 +3,23 @@ const req = require('request');
 const creds = require("./mapsCred.json");
 let mapsQuery = {};
 
-function createAPIrequestURL() {
+function createAPIrequestURL(origin, destination, transportMode) {
+    let urlOrigin = encodeURIComponent(origin).replace('%20', '+');
+    let urlDestination = encodeURIComponent(destination).replace('%20', '+');
     const creds = require('./mapsCred.json');
     console.log(creds.googleMapsKey);
-    let url = `https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=${creds.googleMapsKey}`;
+    let url = `https://maps.googleapis.com/maps/api/directions/json?origin=${urlOrigin}&destination=${urlDestination}&alternative=true&mode=${transportMode}&key=${creds.googleMapsKey}`;
     return url;
 }
 
 
 mapsQuery.get = (origin, destination, timeframe) => {
-    let url = createAPIrequestURL();
+    let url = createAPIrequestURL(origin, destination, "Walking");
     return new Promise( (resolve, reject) => {
         req(url, (err, res, body) => {
             try {
                 let data = JSON.parse(body);
-                console.log(data);
+                console.log(data.routes[0].overview_polyline);
                 resolve(data);
             } catch (error) {
                 reject(error);
@@ -25,6 +27,6 @@ mapsQuery.get = (origin, destination, timeframe) => {
         })
     })
 }
-mapsQuery.get("test","test", 2);
+mapsQuery.get("Lougheed Town Station","Metropolis at metrotown", 2);
 
 module.export = mapsQuery;
