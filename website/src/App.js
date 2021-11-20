1c68de5a8f902d7689321cdbeb50c4f3e93932ad
Yoonha Kim <yoon.ha.kim@sap.com>
2021-11-20 13:24:14 -0800

    googleMap

--- website/src/App.js
+++ website/src/App.js
@@ -3,12 +3,23 @@ import React  from "react";
 import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
 
 
-const WrappedMap = withScriptjs(withGoogleMap((props) => (<GoogleMap defaultZoom={10} defaultCenter={{lat: 43, lng: -79}}/>)));
+//const WrappedMap = withScriptjs(withGoogleMap((props) => (<GoogleMap defaultZoom={10} defaultCenter={{lat: 43, lng: -79}}/>)));
+
+function Map(){
+    return (
+        <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{lat: 49, lng: -123}}
+        />
+    );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
 
     return (
-        <div>
+       /* <div>
             Origin: <input type="text" />
             Destination: <input type="text"/>
             Within Time: <input type="text"/>
@@ -22,6 +33,14 @@ export default function App() {
                 />
             </div>
 
+        </div>*/
+        <div style={{width: '100vw', height: '100vh'}}>
+            <WrappedMap
+                googleMapURL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
+                loadingElement= {<div style={{height: "100%"}} />}
+                containerElement = {<div style={{height: "100%"}} />}
+                mapElement = {<div style={{height: "100%"}} />}
+            />
         </div>
     )
 }
