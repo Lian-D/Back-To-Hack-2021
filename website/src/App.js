b4853b506ec842fe6c6f0a85ba43b9fd042484fc
Joyce Lu <joyce.lu@sap.com>
2021-11-20 12:54:04 -0800

    load google map

--- website/src/App.js
+++ website/src/App.js
@@ -1,18 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from "react";
-import DropDownList from './DropDownList.js';
+import React  from "react";
+import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
 
-function App() {
-  return (
-      <div>
-        Your Location: <input type="text" ></input>
-        Go to: <input type="text"></input>
-          Within Time: <input type="text"></input>
-          <button>Search</button>
 
-      </div>
-  )
+const WrappedMap = withScriptjs(withGoogleMap((props) => (<GoogleMap defaultZoom={10} defaultCenter={{lat: 43, lng: -79}}/>)));
+
+export default function App() {
+
+    return (
+        <div>
+            Origin: <input type="text" />
+            Destination: <input type="text"/>
+            Within Time: <input type="text"/>
+            <button>Search</button>
+            <div style={{width: '100vw', height: '100vh'}}>
+                <WrappedMap
+                            googleMapURL = {`https://www.google.com/maps/@?api=AIzaSyA3q-IyhJlUky394iydh9f-z3Wid9UI-Go&map_action=map`}
+                            loadingElement= {<div style={{height: "100%"}} />}
+                            containerElement = {<div style={{height: "100%"}} />}
+                            mapElement = {<div style={{height: "100%"}} />}
+                />
+            </div>
+
+        </div>
+    )
 }
 
-export default App;
+
+
