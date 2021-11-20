203eb8fa69ea70067a310e335d4bdcccdb507002
Joyce Lu <joyce.lu@sap.com>
2021-11-20 13:03:54 -0800

    remove the key

--- website/src/App.js
+++ website/src/App.js
@@ -15,7 +15,7 @@ export default function App() {
             <button>Search</button>
             <div style={{width: '100vw', height: '100vh'}}>
                 <WrappedMap
-                            googleMapURL = {`https://www.google.com/maps/@?api=AIzaSyA3q-IyhJlUky394iydh9f-z3Wid9UI-Go&map_action=map`}
+                            googleMapURL = {`https://www.google.com/maps/@?api=${key_here}&map_action=map`}
                             loadingElement= {<div style={{height: "100%"}} />}
                             containerElement = {<div style={{height: "100%"}} />}
                             mapElement = {<div style={{height: "100%"}} />}
