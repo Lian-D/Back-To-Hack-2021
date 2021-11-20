87ea94f8aeaa984eecd8d4106f781baa9b50a3f7
Yoonha Kim <yoon.ha.kim@sap.com>
2021-11-20 13:44:41 -0800

    google map api

--- website/src/App.js
+++ website/src/App.js
@@ -36,7 +36,7 @@ export default function App() {
         </div>*/
         <div style={{width: '100vw', height: '100vh'}}>
             <WrappedMap
-                googleMapURL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
+                googleMapURL = 'https://maps.googleapis.com/maps/api/js?key={key_here}&v=3.exp&libraries=geometry,drawing,places'
                 loadingElement= {<div style={{height: "100%"}} />}
                 containerElement = {<div style={{height: "100%"}} />}
                 mapElement = {<div style={{height: "100%"}} />}
